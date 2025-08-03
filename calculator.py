ae2867ae586e021571db3188fb5d4dfbff7ec35e
krishnakumar.loganathan-at-829324643700 <krishnakumar2.loganathan@tataaig.com>
2025-08-03 16:48:02 +0530

    added more tests and full functionality

--- calculator.py
+++ calculator.py
@@ -1,8 +1,39 @@
+import re
+
+
 def add(numbers: str) -> int:
-    if numbers == "":
+    if not numbers:
         return 0
-    parts = list(map( int, numbers.split(",")))
-    if len(parts) == 1:
-        return int(numbers)
-    else:
-        return sum(parts)
+
+    delimiters = [","]
+    if numbers.startswith("//"):
+        delimiter_section, numbers = numbers.split("\n", 1)
+        custom = delimiter_section[2:]
+
+        if custom.startswith("["):
+            delimiters = re.findall(r"\[(.*?)\]", custom)
+        else:
+            delimiters = [custom]
+
+    for delimiter in delimiters:
+        numbers = numbers.replace(delimiter, ",")
+
+    numbers = numbers.replace("\n",",")
+    parts = numbers.split(",")
+    ints = []
+
+    for part in parts:
+        part = part.strip()
+        if not part:
+            continue
+        try:
+            num = int(part)
+            ints.append(num)
+        except ValueError:
+            raise ValueError(f"invalid number found: {part}")
+
+    negatives = [n for n in ints if n < 0]
+    if negatives:
+        raise ValueError(f"negative numbers not allowed {', '.join(map(str, negatives))}")
+
+    return sum(n for n in ints if n <= 1000)
--- test_calculator.py
+++ test_calculator.py
@@ -1,3 +1,5 @@
+import pytest
+
 from calculator import  add
 
 def test_empty_string_returns_zero():
@@ -11,3 +13,42 @@ def test_two_numbers_returns_sum():
 
 def test_unknown_number_of_integers():
     assert add("1,2,3,4,5") == 15
+
+def test_new_line_as_delimiter():
+    assert add("1\n2,3\n4\n5") == 15
+
+def test_custom_delimiter():
+    assert add("//;\n1;2;3") == 6
+
+def test_empty_values_between_commas_are_skipped():
+    assert add("1,,2") == 3
+
+def test_leading_and_trailing_delimiters():
+    assert add(",1,2,") == 3
+
+def test_one_negative_number_throw_not_allowed_error():
+    with pytest.raises(ValueError, match="negative numbers not allowed -5"):
+        add(" 1, -5, 5")
+
+def test_multiple_negative_number_throw_not_allowed_error():
+    with pytest.raises(ValueError, match="negative numbers not allowed -5, -3, -2"):
+        add(" 1, -5, 5, -3, -2")
+
+def test_ignores_greater_than_1000_numbers():
+    assert add("1,2,3,4,1010,5") == 15
+
+def test_invalid_number_raises_exception():
+    with pytest.raises(ValueError, match=r"invalid number found: a"):
+        add("1,2,a")
+
+def test_invalid_number_with_custom_delimiter():
+    with pytest.raises(ValueError, match=r"invalid number found: xyz"):
+        add("//;\n1;xyz;2")
+
+def test_delimiter_of_any_length():
+    assert add("//***\n1***2***3***4***5") == 15
+
+def test_multiple_delimiters():
+    assert add("//[*][#][&][,]\n1*2#3&4\n5")
+
+
